df39bab0558dbcf44c1793130697472f203d3cd3
junta <j1u1n5@gmail.com>
2023-01-12 11:17:37 +0900

    登録フォーム　コントローラ　モデル編集

--- resources/views/product/detail.blade.php
+++ resources/views/product/detail.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="row">
   <div class="col-md-8 col-md-offset-2">
+    <img scr="{{ '/storage/' . $product['img_path']}}" class='w-100 mb-3'/>
       <h2>{{ $product->id}}</h2>
       <span>商品名:{{
         $product->product_name}}</span>
