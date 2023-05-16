a9a3b57b60d8b7829aacd169e792df7e34fae6f5
junoota0115 <j1u1n5@gmail.com>
2023-05-16 16:38:21 +0900

    try catch実装

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Sales;
 use App\Models\Product;
 use Carbon\Carbon;
@@ -16,28 +17,36 @@ class SalesController extends Controller
     //========== update api ==========
     public function update($id)
     {
-        //送られてきたproductのidをsalesテーブルに追加
         
-        //productのidでproductsを検索しstockが0かどうか確認
-        $product_stock = Product::select('products.*')
+        //productのidでproductsを検索し商品があるかどうか確認
+        $select_product = Product::select('products.*')
         ->where('id','=',$id)
         ->first();
         // var_dump($product_stock);
         // ->get();//配列で入ってくる
-        
-        //1つでもあれば、-1してstockを更新する
-        if($product_stock->stock <= 0) {
+
+        DB::beginTransaction();
+        //stockが0か確認
+        try{
+        if($select_product->stock <= 0) {
             echo('在庫がありません！');
+        //1つでもあれば、送られてきたproductのidをsalesテーブルに追加
         }else{
            Sales::insert([
             'product_id' => $id,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
-           $stock = $product_stock->stock -1;
-           Product::where('id','=',$id)->update(['stock' => $stock]);
-           echo('成功しました');        }
-
+        //指定した商品のstockを-1してstockを更新する
+        $product_stock = $select_product->stock -1;
+        Product::where('id','=',$id)->update(['stock' => $product_stock]);
+        echo('成功しました！');
+        }
+        DB::commit();
+        }catch(\Throwable $e){
+           \DB::rollback();
+           abort(500);
+        }
     }
     //========== update api ==========
 
